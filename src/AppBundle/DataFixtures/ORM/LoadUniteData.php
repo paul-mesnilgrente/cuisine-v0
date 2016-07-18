03c77e0eae3711e2cf06d650d57428b8eea124b3
paul-mesnilgrente <paul.mesnilgrente@gmail.com>
2016-07-18 13:59:07 +0200

    fixtures unite ajoute

---
+++ src/AppBundle/DataFixtures/ORM/LoadUniteData.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use AppBundle\Entity\Unite;
+
+class LoadUniteData extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+    private $container;
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    private function addUnite(ObjectManager $manager, $singulier, $pluriel, $abrege) {
+        $unite = new Unite();
+        $unite->setNom($singulier);
+        $unite->setPluriel($pluriel);
+        $unite->setAbrege($abrege);
+        
+        $manager->persist($unite);
+        $manager->flush();
+        
+        $this->addReference('unite-'.strtolower($singulier), $unite);
+    }
+    
+    public function load(ObjectManager $manager)
+    {
+        $this->addUnite($manager, 'milligramme', 'milligrammes', 'mg');
+        $this->addUnite($manager, 'gramme', 'grammes', 'g');
+        $this->addUnite($manager, 'kilogramme', 'kilogrammes', 'kg');
+        $this->addUnite($manager, 'pincée', 'pincées', 'pincée');
+        $this->addUnite($manager, 'poignée', 'poignées', 'poignée');
+        $this->addUnite($manager, 'cuillère à soupe', 'cuillères à soupe', 'c.a.s.');
+        $this->addUnite($manager, 'cuillère à café', 'cuillères à café', 'c.a.c.');
+        $this->addUnite($manager, 'litre', 'litres', 'L');
+        $this->addUnite($manager, 'centilitre', 'centilitres', 'cl');
+        $this->addUnite($manager, 'millilitre', 'millilitres', 'ml');
+        $this->addUnite($manager, 'verre', 'verres', 'verre');
+    }
+    
+    public function getOrder()
+    {
+        return 1;
+    }
+}
